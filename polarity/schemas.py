980c264403dfa0b6987789dfdca1ac28bfe8e712
GS <gsfernandes81@gmail.com>
2022-06-18 16:43:41 +0100

    Initial database setup with alembic

---
+++ polarity/schemas.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.sql.schema import Column
+from sqlalchemy.sql.sqltypes import String
+
+Base = declarative_base()
+
+
+class Commands(Base):
+    __tablename__ = "Commands"
+    __mapper_args__ = {"eager_defaults": True}
+    name = Column("name", String, primary_key=True)
+    description = Column("description", String)
+    text = Column("text", String)
+
+    def __init__(self, name, description, text):
+        super().__init__()
+        self.name = name
+        self.description = description
+        self.text = text
