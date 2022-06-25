646ba505bc637dca940dc09a38f6efc71335e848
GS <gsfernandes81@gmail.com>
2022-06-25 15:50:15 +0100

    Better segregation of code

---
+++ polarity/utils.py
@@ -0,0 +1,69 @@
+import logging
+import re
+
+import aiohttp
+import hikari
+import lightbulb
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql.expression import select
+
+from . import cfg
+from .schemas import Commands
+
+db_engine = create_async_engine(cfg.db_url_async)
+db_session = sessionmaker(db_engine, **cfg.db_session_kwargs)
+
+
+url_regex = re.compile(
+    "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+)
+
+
+class RefreshCmdListEvent(hikari.Event):
+    def __init__(self, bot: hikari.GatewayBot, sync: bool = True):
+        super().__init__()
+        # Whether to run the sync_application_commands method of the app
+        self.bot = bot
+        self.sync = sync
+
+    def app(self):
+        return self.bot
+
+    def dispatch(self):
+        self.bot.event_manager.dispatch(self)
+
+
+async def user_command(ctx: lightbulb.Context):
+    async with db_session() as session:
+        async with session.begin():
+            command = (
+                await session.execute(
+                    select(Commands).where(Commands.name == ctx.command.name)
+                )
+            ).fetchone()[0]
+    text = command.response.strip()
+    # Follow the redirects, check the extension, download only if it is a jgp
+    # Above to be implemented
+    links = url_regex.findall(text)
+    redirected_links = []
+    redirected_text = url_regex.sub("{}", text)
+    async with aiohttp.ClientSession() as session:
+        for link in links:
+            async with session.get(link) as response:
+                redirected_links.append(response.url)
+                logging.debug(
+                    "Replacing link: {} with redirect: {}".format(
+                        link, redirected_links[-1]
+                    )
+                )
+    redirected_text = redirected_text.format(*redirected_links)
+
+    await ctx.respond(redirected_text)
+
+
+def db_command_to_lb_user_command(command: Commands):
+    # Needs an open db session watching command
+    return lightbulb.command(command.name, command.description, auto_defer=True)(
+        lightbulb.implements(lightbulb.SlashCommand)(user_command)
+    )
