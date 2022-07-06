51fecba4ab3825e242414c99cfb4741d11116307
GS <gsfernandes81@gmail.com>
2022-07-06 19:30:56 +0100

    Move database definitions to schemas.py

---
+++ polarity/schemas.py
@@ -0,0 +1,50 @@
+from sqlalchemy import BigInteger, Boolean, Integer, String
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.sql.schema import Column
+
+from . import cfg
+
+Base = declarative_base()
+db_engine = create_async_engine(cfg.db_url_async)
+db_session = sessionmaker(db_engine, **cfg.db_session_kwargs)
+
+
+class LostSectorPostSettings(Base):
+    __tablename__ = "lostsectorpostsettings"
+    __mapper_args__ = {"eager_defaults": True}
+    id = Column("id", Integer, primary_key=True)
+    autoannounce_enabled = Column(
+        "autoannounce_enabled", Boolean, default=True, server_default="t"
+    )
+
+    def __init__(self, id, autoannounce_enabled=True):
+        self.id = id
+        self.autoannounce_enabled = autoannounce_enabled
+
+
+class LostSectorAutopostChannel(Base):
+    __tablename__ = "lostsectorautopostchannel"
+    __mapper_args__ = {"eager_defaults": True}
+    id = Column("id", BigInteger, primary_key=True)
+    server_id = Column("server_id", BigInteger)
+    enabled = Column("enabled", Boolean)
+
+    def __init__(self, id: int, server_id: int, enabled: bool):
+        self.id = id
+        self.server_id = server_id
+        self.enabled = enabled
+
+
+class Commands(Base):
+    __tablename__ = "commands"
+    __mapper_args__ = {"eager_defaults": True}
+    name = Column("name", String, primary_key=True)
+    description = Column("description", String)
+    response = Column("response", String)
+
+    def __init__(self, name, description, response):
+        super().__init__()
+        self.name = name
+        self.description = description
+        self.response = response
