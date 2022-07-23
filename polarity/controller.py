d51898d6a5e7cdbecc268d3024414024a8ec0c39
GS <gsfernandes81@gmail.com>
2022-07-23 21:47:44 +0100

    Move large parts of code to orm classes
    Moved announcer functions, embed creation and associated code to schemas
    Also fixed bug with url watcher code that prevented saving
    the redirected url. The displayed symptom was Xur autoposts always
    triggering immediately on weekend reset signal
    The fix was adding db_session_.add(self) before modifying params

--- polarity/controller.py
+++ polarity/controller.py
@@ -4,15 +4,10 @@ from typing import List
 
 import lightbulb
 from sqlalchemy import select
-import datetime as dt
 
-from polarity.schemas import LostSectorPostSettings, XurAutopostChannel, XurPostSettings
-from polarity.user_commands import get_xur_text
-from polarity.utils import operation_timer
-
-from . import cfg
-from .schemas import XurPostSettings, db_session
-from .autoannounce import XurSignal, _edit_embedded_message
+from . import cfg, schemas
+from .autoannounce import XurSignal
+from .utils import _edit_embedded_message, db_session, operation_timer
 
 
 @lightbulb.add_checks(lightbulb.checks.has_roles(cfg.admin_role))
@@ -49,9 +44,9 @@ async def ls_announcements(ctx: lightbulb.Context):
     option = True if ctx.options.option.lower() == "enable" else False
     async with db_session() as session:
         async with session.begin():
-            settings = await session.get(LostSectorPostSettings, 0)
+            settings = await session.get(schemas.LostSectorPostSettings, 0)
             if settings is None:
-                settings = LostSectorPostSettings(0, option)
+                settings = schemas.LostSectorPostSettings(0, option)
                 session.add(settings)
             else:
                 settings.autoannounce_enabled = option
@@ -94,9 +89,9 @@ async def xur_autoposts(ctx: lightbulb.Context):
     option = True if ctx.options.option.lower() == "enable" else False
     async with db_session() as session:
         async with session.begin():
-            settings = await session.get(XurPostSettings, 0)
+            settings = await session.get(schemas.XurPostSettings, 0)
             if settings is None:
-                settings = XurPostSettings(0, autoannounce_enabled=option)
+                settings = schemas.XurPostSettings(0, autoannounce_enabled=option)
                 session.add(settings)
             else:
                 settings.autoannounce_enabled = option
@@ -123,7 +118,9 @@ async def xur_gfx_url(ctx: lightbulb.Context):
     url = ctx.options.url.lower() if ctx.options.url is not None else None
     async with db_session() as session:
         async with session.begin():
-            settings: XurPostSettings = await session.get(XurPostSettings, 0)
+            settings: schemas.XurPostSettings = await session.get(
+                schemas.XurPostSettings, 0
+            )
             if ctx.options.url is None:
                 await ctx.respond(
                     (
@@ -136,7 +133,7 @@ async def xur_gfx_url(ctx: lightbulb.Context):
                 )
                 return
             if settings is None:
-                settings = XurPostSettings(0)
+                settings = schemas.XurPostSettings(0)
                 settings.url = url
                 await settings.initialise_url_params()
                 session.add(settings)
@@ -163,7 +160,9 @@ async def xur_post_url(ctx: lightbulb.Context):
     url = ctx.options.url.lower() if ctx.options.url is not None else None
     async with db_session() as session:
         async with session.begin():
-            settings: XurPostSettings = await session.get(XurPostSettings, 0)
+            settings: schemas.XurPostSettings = await session.get(
+                schemas.XurPostSettings, 0
+            )
             if ctx.options.url is None:
                 await ctx.respond(
                     (
@@ -176,7 +175,7 @@ async def xur_post_url(ctx: lightbulb.Context):
                 )
                 return
             if settings is None:
-                settings = XurPostSettings(0)
+                settings = schemas.XurPostSettings(0)
                 settings.post_url = url
                 session.add(settings)
             else:
@@ -204,24 +203,28 @@ async def xur_rectify_announcement(ctx: lightbulb.Context):
     change = ctx.options.change if ctx.options.change else ""
     async with db_session() as session:
         async with session.begin():
-            settings: XurPostSettings = await session.get(XurPostSettings, 0)
+            settings: schemas.XurPostSettings = await session.get(
+                schemas.XurPostSettings, 0
+            )
             if settings is None:
                 await ctx.respond("Please enable xur autoposts before using this cmd")
             channel_record_list = (
                 await session.execute(
-                    select(XurAutopostChannel).where(XurAutopostChannel.enabled == True)
+                    select(schemas.XurAutopostChannel).where(
+                        schemas.XurAutopostChannel.enabled == True
+                    )
                 )
             ).fetchall()
             channel_record_list = (
                 [] if channel_record_list is None else channel_record_list
             )
-            channel_record_list: List[XurAutopostChannel] = [
+            channel_record_list: List[schemas.XurAutopostChannel] = [
                 channel[0] for channel in channel_record_list
             ]
         logging.info("Correcting xur posts")
         with operation_timer("Xur announce correction"):
             await ctx.respond("Correcting posts now")
-            embed = await get_xur_text(
+            embed = await settings.get_announce_embed(
                 settings.url,
                 settings.post_url,
                 change,
--- polarity/main.py
+++ polarity/main.py
@@ -7,7 +7,7 @@ from . import cfg, controller, debug_commands, user_commands
 from .autoannounce import arm
 
 # Note: Alembic's env.py is set up to import Base from polarity.main
-from .schemas import Base
+from .utils import Base
 
 uvloop.install()
 bot: lightbulb.BotApp = lightbulb.BotApp(**cfg.lightbulb_params)
